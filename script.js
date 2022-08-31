ed07f6949e4e2493a71b0a52a0d724bedeaffa74
Kyle G <garnerk10@gmail.comgit config --global user.email garnerk10@gmail.com>
2022-08-31 13:31:00 -0400

    added fade effect to title

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 //Welcome button
 let welcome = document.getElementById("welcome");
+let welc_text = document.getElementById("welc_text")
 
+//Fade welcome button
+function fadeWelcome() {
+    let fo = setInterval(fadeOut, 10);
+    var opa = 1;
+
+    function fadeOut(){
+        if(opa <= 0){
+            clearInterval(fo);
+            fadeIn();
+        } else {
+            opa -= 0.01;
+            welc_text.style.opacity = opa;
+        }
+    };
+
+    function fadeIn(){
+        welc_text.innerHTML = "Kyle Garner's Portfolio";
+        let fin = setInterval(fadeI, 10);
+        function fadeI(){
+        if(opa >= 1){
+            clearInterval(fin);
+        } else {
+            opa += 0.01;
+            welc_text.style.opacity = opa;
+        };
+        };
+    };
+};
+
+//Expand welcome button to width of page and become title
 function expand() {
     let e = setInterval(exp, 5);
     let posx = 10;
@@ -9,6 +40,7 @@ function expand() {
         if(posx >= 100){
             welcome.style.width = "100%";
             clearInterval(e);
+            fadeWelcome();
         } else {
             posx += 0.5;
             welcome.style.width = posx+'%'
@@ -16,6 +48,7 @@ function expand() {
     }
 };
 
+//Move welcome button to top of page
 welcome.onclick = function(){
     let t = setInterval(move, 5);
     let pos = 50;
